75b2fcf04e53df82a6165ac5c0c15a38372d610c
CoadyTech <will@coady.tech>
2022-09-18 23:48:04 +0100

    Implement alpha Lambda check logic

--- amplify/backend/function/spfcheckiod043eb99/src/index.js
+++ amplify/backend/function/spfcheckiod043eb99/src/index.js
@@ -1,16 +1,27 @@
-
-
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
+let spfValidator = require("spf-validator");
+let resultBool = null;
+
+function getResult(hasRecords) {
+    resultBool = hasRecords;
+}
+
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
+    let domainValidated = new spfValidator(event.domain);
+    domainValidated.hasRecords((err, hasRecords) => getResult(hasRecords));
+    console.log(resultBool);
     return {
         statusCode: 200,
       headers: {
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Headers": "*"
       }, 
-        body: JSON.stringify({spfEnabled: true, spfOutput: "string", domain: "string"}),
+        body: JSON.stringify({spfEnabled: resultBool, spfOutput: "string", domain: event.domain}),
     };
 };
+
+
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "spfcheckiod043eb99": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "spfcheckiolayer119c8661": {
+            "Arn": "string"
+        }
+    },
+    "api": {
+        "apiSpfCheck": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
