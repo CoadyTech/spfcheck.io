c902bf7c37d1ebcf74c5de071389d69b890045ce
CoadyTech <will@coady.tech>
2022-09-19 22:49:25 +0100

    Implement basic SPF checking with Lambda

--- amplify/backend/function/spfcheckiod043eb99/src/index.js
+++ amplify/backend/function/spfcheckiod043eb99/src/index.js
@@ -2,26 +2,23 @@
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 
-let spfValidator = require("spf-validator");
-let resultBool = null;
-
-function getResult(hasRecords) {
-    resultBool = hasRecords;
-}
-
-exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    let domainValidated = new spfValidator(event.domain);
-    domainValidated.hasRecords((err, hasRecords) => getResult(hasRecords));
-    console.log(resultBool);
-    return {
-        statusCode: 200,
-      headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "*"
-      }, 
-        body: JSON.stringify({spfEnabled: resultBool, spfOutput: "string", domain: event.domain}),
-    };
-};
-
-
+ let spfValidator = require("spf-validator");
+ let resultBool = null;
+ 
+ function getResult(hasRecords) {
+     resultBool = hasRecords;
+ }
+ 
+ exports.handler = async (event) => {
+     let jsonbody = JSON.parse(event.body);
+     let domainValidated = new spfValidator(jsonbody.domain);
+     await domainValidated.hasRecords((err, hasRecords) => getResult(hasRecords));
+     return {
+         statusCode: 200,
+       headers: {
+           "Access-Control-Allow-Origin": "*",
+           "Access-Control-Allow-Headers": "*"
+       }, 
+         body: JSON.stringify({spfEnabled: resultBool, spfOutput: "string", domain: jsonbody.domain}),
+     };
+ };
