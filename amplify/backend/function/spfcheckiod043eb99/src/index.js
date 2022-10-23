94ce67dff2cfc309e91fcc77fbfcea68be14a37d
CoadyTech <will@coady.tech>
2022-10-23 19:28:44 +0100

    Fixed intermittent 'null' issue
    - Found the Lambda was finishing before a callback function could be called.
    - Implemented a promise on the server-side Lambda to replace the callback function.
    - Improved client-side code flow with async/await instead of chaining together functions.

--- amplify/backend/function/spfcheckiod043eb99/src/index.js
+++ amplify/backend/function/spfcheckiod043eb99/src/index.js
@@ -1,24 +1,34 @@
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+let spfValidator = require("spf-validator");
 
- let spfValidator = require("spf-validator");
- let resultBool = null;
- 
- function getResult(hasRecords) {
-     resultBool = hasRecords;
- }
- 
- exports.handler = async (event) => {
-     let jsonbody = JSON.parse(event.body);
-     let domainValidated = new spfValidator(jsonbody.domain);
-     await domainValidated.hasRecords((err, hasRecords) => getResult(hasRecords));
-     return {
-         statusCode: 200,
-       headers: {
-           "Access-Control-Allow-Origin": "*",
-           "Access-Control-Allow-Headers": "*"
-       }, 
-         body: JSON.stringify({spfEnabled: resultBool, spfOutput: "string", domain: jsonbody.domain}),
-     };
- };
+function spfCheck(domainValidated) {
+  return new Promise((resolve, reject) => {
+    domainValidated.hasRecords((err, hasRecords) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(hasRecords);
+      }
+    });
+  });
+}
+
+exports.handler = async (event) => {
+  let jsonbody = JSON.parse(event.body);
+  let domainValidated = await new spfValidator(jsonbody.domain);
+  let resultBool = await spfCheck(domainValidated);
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+    },
+    body: JSON.stringify({
+      spfEnabled: resultBool,
+      spfOutput: "string",
+      domain: jsonbody.domain,
+    }),
+  };
+};
--- frontend/logic.js
+++ frontend/logic.js
@@ -1,57 +1,82 @@
-/* Check if JSON input is a valid domain */ 
-function domainInputValidator(domain) { 
-    var re = new RegExp(/^((?:(?:(?:\w[\.\-\+]?)*)\w)+)((?:(?:(?:\w[\.\-\+]?){0,62})\w)+)\.(\w{2,6})$/); 
-    return domain.match(re);
-} 
+let domainResultJson = null;
+
+/* Check if JSON input is a valid domain */
+function domainInputValidator(domain) {
+  var re = new RegExp(
+    /^((?:(?:(?:\w[\.\-\+]?)*)\w)+)((?:(?:(?:\w[\.\-\+]?){0,62})\w)+)\.(\w{2,6})$/
+  );
+  return domain.match(re);
+}
+
+let jsonPromise = null;
 
 /* Dump domain to the log and then call vewportOutput for DOM inject */
 async function domainSpfCheckApi(domainToCheck) {
-        document.getElementById('loader').style.display = 'block';
-        const rawResponse = await fetch('https://cqi25h9ctk.execute-api.eu-west-2.amazonaws.com/main', {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ domain: domainToCheck })
-        });
-        const content = await rawResponse.json();
-        document.getElementById('loader').style.display = 'none';
-        return content;
-      };
+  document.getElementById("loader").style.display = "block";
+  const rawResponse = fetch(
+    "https://cqi25h9ctk.execute-api.eu-west-2.amazonaws.com/main",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ domain: domainToCheck }),
+    }
+  );
+
+  await rawResponse
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      domainResultJson = data;
+    })
+    .catch((error) => {
+      console.error(`Could not get SPF record: ${error}`);
+    });
+
+  document.getElementById("loader").style.display = "none";
+}
 
 /* Logic for DOM injecttion */
 function domainResultDomManipulator(domainResultJson) {
-    if (domainResultJson.spfEnabled) {
-        document.getElementById('domain-result-success')?.remove();
-        document.getElementById('domain-result-spfrecord')?.remove();
-        let domainResult = document.createElement('p');
-        domainResult.id = 'domain-result-success';
-        domainResult.innerHTML = `Good news! SPF is enabled for <strong>${domainResultJson.domain}</strong>`;
-        document.getElementById('domain-result').appendChild(domainResult);
-        let domainResultRaw = document.createElement('p');
-        domainResultRaw.id = 'domain-result-spfrecord';
-        domainResultRaw.innerHTML = (domainResultJson.spfOutput);
-        document.getElementById('domain-result').appendChild(domainResultRaw);
-    } else {
-        document.getElementById('domain-result-fail')?.remove();
-        domainResult.id = 'domain-result-fail';
-        domainResult.innerHTML = `SPF is not enabled for <strong>${domainResultJson.domain}</strong>`;
-        document.getElementById('domain-result').appendChild(domainResult);
-    }
+  document.getElementById("domain-result-success")?.remove();
+  document.getElementById("domain-result-spfrecord")?.remove();
+  document.getElementById("domain-result-fail")?.remove();
+  let domainResult = document.createElement("p");
+  if (domainResultJson.spfEnabled) {
+    document.getElementById("domain-result-success")?.remove();
+    document.getElementById("domain-result-spfrecord")?.remove();
+    domainResult.id = "domain-result-success";
+    domainResult.innerHTML = `Good news! SPF is enabled for <strong>${domainResultJson.domain}</strong>`;
+    document.getElementById("domain-result").appendChild(domainResult);
+    let domainResultRaw = document.createElement("p");
+    domainResultRaw.id = "domain-result-spfrecord";
+    domainResultRaw.innerHTML = domainResultJson.spfOutput;
+    document.getElementById("domain-result").appendChild(domainResultRaw);
+  } else {
+    document.getElementById("domain-result-fail")?.remove();
+    domainResult.id = "domain-result-fail";
+    domainResult.innerHTML = `SPF is not enabled for <strong>${domainResultJson.domain}</strong>`;
+    document.getElementById("domain-result").appendChild(domainResult);
+  }
 }
 
 async function spfCheck() {
-    let domainToCheck = document.getElementById("domain-name").value;
-    if (domainInputValidator(domainToCheck)) {
-        let domainResultJson = await domainSpfCheckApi(domainToCheck);
-        domainResultDomManipulator(domainResultJson);
-    } else {
-            alert("Please enter a valid domain name.");
-    }
+  let domainToCheck = document.getElementById("domain-name").value;
+  if (domainInputValidator(domainToCheck)) {
+    await domainSpfCheckApi(domainToCheck);
+    domainResultDomManipulator(domainResultJson);
+  } else {
+    alert("Please enter a valid domain name.");
+  }
 }
 
 /* Event listener for submit button press. Calls function domainSubmit */
 let btn = document.getElementById("domain-submit");
-btn.addEventListener('click', spfCheck);
-
+btn.addEventListener("click", spfCheck);
