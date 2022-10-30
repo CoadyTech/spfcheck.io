a3950e3fdb371748ce154a70dfe1feb74c2d0f32
CoadyTech <will@coady.tech>
2022-10-30 18:36:30 +0000

    Security: Implement server-side input validation
    Use validator.js to check the domain input is valid.

--- amplify/backend/function/spfcheckiod043eb99/src/index.js
+++ amplify/backend/function/spfcheckiod043eb99/src/index.js
@@ -2,6 +2,7 @@
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 let spfValidator = require("spf-validator");
+let validator = require('validator');
 
 function spfCheck(domainValidated) {
   return new Promise((resolve, reject) => {
@@ -15,20 +16,40 @@ function spfCheck(domainValidated) {
   });
 }
 
+function inputValidator(domain) {
+  let options = { require_tld: false, allow_underscores: false, allow_trailing_dot: false, allow_numeric_tld: false, allow_wildcard: false };
+  return validator.isFQDN(domain, options);
+}
+
 exports.handler = async (event) => {
   let jsonbody = JSON.parse(event.body);
-  let domainValidated = await new spfValidator(jsonbody.domain);
-  let resultBool = await spfCheck(domainValidated);
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "*",
-    },
-    body: JSON.stringify({
-      spfEnabled: resultBool,
-      spfOutput: "string",
-      domain: jsonbody.domain,
-    }),
+  if (inputValidator(jsonbody.domain)) {
+    let domainValidated = await new spfValidator(jsonbody.domain);
+    let resultBool = await spfCheck(domainValidated);
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+      },
+      body: JSON.stringify({
+        spfEnabled: resultBool,
+        spfOutput: "string",
+        domain: jsonbody.domain,
+      }),
+    };
+  } else {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+      },
+      body: JSON.stringify({
+        error: "Invalid input",
+      }),
+    }
   };
+
+
 };
